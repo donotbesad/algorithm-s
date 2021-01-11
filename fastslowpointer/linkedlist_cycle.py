c22701c3cbf2c736f626fdfb3a249e5898d7b77e
donotbesad <dmtry.artemenko@gmail.com>
2021-01-11 21:11:40 +0200

    LinkedList Cycle (easy) (LeetCode 141. Linked List Cycle)

---
+++ fastslowpointer/linkedlist_cycle.py
@@ -0,0 +1,36 @@
+# -------------------------------------------------------------
+#           LinkedList Cycle (easy)
+# Given the head of a Singly LinkedList, write a function to determine
+# if the LinkedList has a cycle in it or not.
+#
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+# -------------------------------------------------------------
+from fastslowpointer.Node import Node
+
+
+def has_cycle(root):
+    slow = root
+    fast = root
+    while fast is not None and fast.next is not None:
+        slow = slow.next
+        fast = fast.next.next
+        while slow == fast:
+            return True
+    return False
+
+
+if __name__ == '__main__':
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+    head.next.next.next.next.next = Node(6)
+    print("LinkedList has cycle: " + str(has_cycle(head)))
+
+    head.next.next.next.next.next.next = head.next.next
+    print("LinkedList has cycle: " + str(has_cycle(head)))
+
+    head.next.next.next.next.next.next = head.next.next.next
+    print("LinkedList has cycle: " + str(has_cycle(head)))
