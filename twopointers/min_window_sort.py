61eb5e699d4e0ab01dcf770d33ef065c0af315e5
donotbesad <dmtry.artemenko@gmail.com>
2021-01-21 23:33:41 +0200

    Minimum Window Sort (medium) 581. Shortest Unsorted Continuous Subarray Time O(n) Space O(1)

--- twopointers/min_window_sort.py
+++ twopointers/min_window_sort.py
@@ -21,8 +21,42 @@ def shortest_window_sort(arr):
         return 0
 
 
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+def shortest_window_sort_improved(arr):
+    start, end = 0, len(arr) - 1
+
+    while start < len(arr) - 1 and arr[start] <= arr[start + 1]:
+        start += 1
+
+    if start == len(arr) - 1:
+        return 0
+
+    while end > 0 and arr[end] >= arr[end - 1]:
+        end -= 1
+
+    sub_max = float('-inf')
+    sub_min = float('inf')
+    for i in range(start, end + 1):
+        sub_max = max(sub_max, arr[i])
+        sub_min = min(sub_min, arr[i])
+
+    while start > 0 and arr[start - 1] > sub_min:
+        start -= 1
+
+    while end < len(arr) - 1 and arr[end + 1] < sub_min:
+        end += 1
+
+    return end - start + 1
+
+
 if __name__ == '__main__':
     print(shortest_window_sort([1, 2, 5, 3, 7, 10, 9, 12]))
     print(shortest_window_sort([1, 3, 2, 0, -1, 7, 10]))
     print(shortest_window_sort([1, 2, 3]))
     print(shortest_window_sort([3, 2, 1]))
+    print('Improved Time O(n) Space O(1)')
+    print(shortest_window_sort_improved([1, 2, 5, 3, 7, 10, 9, 12]))
+    print(shortest_window_sort_improved([1, 3, 2, 0, -1, 7, 10]))
+    print(shortest_window_sort_improved([1, 2, 3]))
+    print(shortest_window_sort_improved([3, 2, 1]))
