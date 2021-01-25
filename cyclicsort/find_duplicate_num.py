67136c193200b404f0b7bea42c8a58ab02828b6d
donotbesad <dmtry.artemenko@gmail.com>
2021-01-25 22:19:12 +0200

    Find the Duplicate Number (easy) (LeetCode 287. Find the Duplicate Number)

--- cyclicsort/find_duplicate_num.py
+++ cyclicsort/find_duplicate_num.py
@@ -1,16 +1,15 @@
 # -------------------------------------------------------------
-#           Find all Missing Numbers (easy)
-#
+#           Find the Duplicate Number (easy)
 # We are given an unsorted array containing ‘n+1’ numbers taken from the range 1 to ‘n’.
-# The array has only one duplicate but it can be repeated multiple times.
+# The array has only one duplicate but it can be repeated multiple times
+#
 # Time Complexity: O(n)
 # Space Complexity: O(1)
 # -------------------------------------------------------------
-def find_miss_nums(nums):
+def find_duplicate(nums):
     i = 0
-    res = []
     while i < len(nums):
-        j = nums[i] - 1
+        j = nums[i]
         if nums[i] != nums[j]:
             nums[i], nums[j] = nums[j], nums[i]
         else:
@@ -18,12 +17,13 @@ def find_miss_nums(nums):
 
     for i in range(len(nums)):
         if nums[i] != i + 1:
-            res.append(i + 1)
+            if nums[i] == nums[nums[i]]:
+                return nums[i]
 
-    return res
+    return -1
 
 
 if __name__ == '__main__':
-    print(find_miss_nums(nums=[2, 3, 1, 8, 2, 3, 5, 1]))
-    print(find_miss_nums(nums=[2, 4, 1, 2]))
-    print(find_miss_nums(nums=[2, 3, 2, 1]))
+    print(find_duplicate(nums=[1, 4, 4, 3, 2]))
+    print(find_duplicate(nums=[2, 1, 3, 3, 5, 4]))
+    print(find_duplicate(nums=[2, 4, 1, 4, 4]))
